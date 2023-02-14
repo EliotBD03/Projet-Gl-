9c18587dfd0ebd2f24a0dafa78708bf515133105
EliotBD03 <julienladeuze@outlook.fr>
2023-02-14 18:02:25 +0100

    set LanguageManager + test

--- server/app/src/main/java/main/be/ac/umons/g02/database/LanguageManager.java
+++ server/app/src/main/java/main/be/ac/umons/g02/database/LanguageManager.java
@@ -1,4 +1,59 @@
 package main.be.ac.umons.g02.database;
 
-public class LanguageManager {
+import java.util.ArrayList;
+
+public class LanguageManager
+{
+    private boolean doesTheLanguageNotExist(String userId, String language)
+    {
+        DB.getInstance().executeQuery("SELECT EXISTS(SELECT * FROM language WHERE saved_language='"+language+"') AS 'result'", true);
+        return Integer.parseInt(DB.getInstance().getResults(new String[]{"result"}).get(0).get(0)) != 1;
+    }
+
+    public String getLanguage(String userId)
+    {
+        DB.getInstance().executeQuery("SELECT saved_language FROM language WHERE current_language=1 AND id=" + userId, true);
+        ArrayList<ArrayList<String>> results = DB.getInstance().getResults(new String[] {"saved_language"});
+
+        if(results.get(0).size() == 0)
+            return null;
+
+        return results.get(0).get(0);
+    }
+
+    public String getFavouriteLanguage(String userId)
+    {
+        DB.getInstance().executeQuery("SELECT saved_language FROM language WHERE favourite_language=1 AND id=" + userId, true);
+        return DB.getInstance().getResults(new String[] {"saved_language"}).get(0).get(0);
+    }
+
+    public ArrayList<String> getAllLanguages(String userId)
+    {
+        DB.getInstance().executeQuery("SELECT saved_language FROM language WHERE id=" + userId,true);
+        return DB.getInstance().getResults(new String[] {"saved_language"}).get(0);
+    }
+
+    public void addLanguage(String userId, String language)
+    {
+        DB.getInstance().executeQuery("UPDATE language SET current_language=0 WHERE current_language=1",false);
+        DB.getInstance().executeQuery("INSERT INTO language(id,saved_language,favourite_language,current_language) VALUES ("+userId+",'"+language+"',0,1)", false);
+    }
+
+    public void changeLanguage(String userId, String language)
+    {
+        if(doesTheLanguageNotExist(userId, language))
+            addLanguage(userId, language);
+
+        DB.getInstance().executeQuery("UPDATE language SET current_language=0 WHERE current_language=1", false);
+        DB.getInstance().executeQuery("UPDATE language SET current_language=1 WHERE saved_language='"+ language +"'", false);
+    }
+
+    public void changeFavouriteLanguage(String userId, String language)
+    {
+        if(doesTheLanguageNotExist(userId, language))
+            addLanguage(userId,language);
+        
+        DB.getInstance().executeQuery("UPDATE language SET favourite_language=0 WHERE favourite_language=1", false);
+        DB.getInstance().executeQuery("UPDATE language SET favourite_language=1 WHERE saved_language='"+language+"'",false);
+    }
 }
---
+++ server/app/src/test/java/main/be/ac/umons/g02/database/LanguageManagerTest.java
@@ -0,0 +1,61 @@
+package main.be.ac.umons.g02.database;
+
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LanguageManagerTest {
+
+    @BeforeAll
+    public static void setup()
+    {
+        try //If LogManagerTest has already been tested
+        {
+            new LogManager().saveAccount("test@gmail.com", "password", true, "name", "english");
+        }
+        catch (Exception e)
+        {
+            System.out.println("account already exists");
+        }
+    }
+
+    @Test
+    void getLanguage()
+    {
+        assertEquals(new LanguageManager().getLanguage("1"), "english");
+    }
+
+    @Test
+    void getFavouriteLanguage()
+    {
+        assertNotEquals(new LanguageManager().getFavouriteLanguage("1"), "dutch");
+    }
+
+    @Test
+    void getAllLanguages()
+    {
+        assertEquals(new LanguageManager().getAllLanguages("1").get(0), "english");
+    }
+
+    @Test
+    void addLanguage()
+    {
+        LanguageManager languageManager = new LanguageManager();
+        assertNotEquals(languageManager.getLanguage("5"), "german");
+        languageManager.addLanguage("5", "german");
+        assertEquals(languageManager.getLanguage("5"), "german");
+    }
+
+    @Test
+    void changeLanguage()
+    {
+        LanguageManager languageManager = new LanguageManager();
+        assertNotEquals(languageManager.getLanguage("1"), "english");
+        languageManager.changeLanguage("1", "english");
+        assertEquals(languageManager.getLanguage("1"), "english");
+    }
+}
