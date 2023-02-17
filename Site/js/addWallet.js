cd29edb9e0648715df1e9e4c5edf5634fcc64a39
ClaireDH3 <clairedhaene3@gmail.com>
2023-02-17 16:56:53 +0100

    Fin du AddWallet + tests effectués

--- Site/js/addWallet.js
+++ Site/js/addWallet.js
@@ -6,20 +6,23 @@ new Vue({
       address: ''
     }},
     //Post //token = ? (dans le lien) checkaccount faire .token
-    created() {
-        const requestOptions = {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ name: this.name, address: this.address })
-          };
-          fetch("https://babawallet.alwaysdata.net:8300/api/client/?/wallets", requestOptions)
-            .then(response => response.json())
-        },
     methods: {
-        //Il faudra vérifier qu'aucun wallet se crée dans ce cas
         checkArgs(){
           if(!this.name) alert("Please enter your name");
           if(!this.address) alert("Please enter your address");
+          else return true;
+        },
+        post(){
+          if(this.checkArgs())
+          {
+            const requestOptions = {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({ name: this.name, address: this.address })
+            };
+            fetch("https://babawallet.alwaysdata.net:8300/api/client/?/wallets", requestOptions)
+              .then(response => response.json())
+          }
         }
       }
     });
--- test/js/addWallet.js
+++ test/js/addWallet.js
@@ -3,22 +3,23 @@ new Vue({
     data(){
     return{
       name: '',
-      address: ''
+      address: '',
+      listWallet: [
+        { name: "Item 1", nameOwner: "BOb", address: "Rue ll", lastConsumptionOfWater: 10, lastConsumptionOfGas: 44, lastConsumptionOfElectricity: 90, listContracts: [{nom: "Engie", conso: "10000", prix : "400000"}, {nom: "paee", conso: "9000", prix : "5000"}]},
+        
+        { name: "Item 2", nameOwner: "Bobby", address: "Rue nn", lastConsumptionOfWater: 20, lastConsumptionOfGas: 30, lastConsumptionOfElectricity: 100, listContracts: [{nom: "Watersss", conso: "60000", prix : "9000"}]},
+      ]
     }},
-    //Post //token = ? (dans le lien) checkaccount faire .token
-    created() {
-        const requestOptions = {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ name: this.name, address: this.address })
-          };
-          fetch("https://babawallet.alwaysdata.net:8300/api/client/?/wallets", requestOptions)
-            .then(response => response.json())
-        },
     methods: {
         checkArgs(){
           if(!this.name) alert("Please enter your name");
           if(!this.address) alert("Please enter your address");
+          else return true;
+        }, //l'idée était juste de voir si cela fonctionnait :)
+        addWallet(name, address){
+          if (this.checkArgs()) {
+            this.listWallet.push(name + address);
+            console.log(this.listWallet[2]);}
         }
       }
     });
