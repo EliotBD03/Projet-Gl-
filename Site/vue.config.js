f337c8c7902750c5c23d2973a77dc1a4440066a5
Maxime Dupuis <212107@umons.ac.be>
2023-03-15 09:17:46 +0100

    Change site title

--- Site/vue.config.js
+++ Site/vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: config => { config.plugin('html').tap(args => { args[0].title = 'BABA Wallet'; return args; }); },
 
   pluginOptions: {
     i18n: {
