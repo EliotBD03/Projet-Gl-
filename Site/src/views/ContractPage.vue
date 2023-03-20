3a734deacc741262b4704ab1a6d2df414cc6784d
Maxime Dupuis <212107@umons.ac.be>
2023-03-20 15:04:01 +0100

    Update ContractPAge.vue

--- Site/src/views/ContractPage.vue
+++ Site/src/views/ContractPage.vue
@@ -1,34 +1,37 @@
 <template>
-  <!--Idée de la page des contrats (clients)-->
   <div class="main">
     <div class="header">
       <MainHeader text="header.contracts"/>
     </div>
-    <div class="searchbar">
-      <SearchBar/>
+    <div class="allcards">
+      <div class=cards v-for="contract in listContracts" :key="contract.id">
+        <div class="texte">
+          <p class="name"> {{ contract.name }} :</p>
+          <p> {{ contract.nameOwner }}</p>
+          <p> {{ contract.address }}</p>
+        </div>
+        <div @click.prevent.left="seeMore(contracts)">
+          <GoButton text="button.go" :colore="'#34c98e'"/>
+        </div>
+      </div>
+      <div v-if="notLastPage()" @click.prevent.left="loader()">
+        <GoButton text="See more contracts" :colore="'#B1B9FC'"/>
+      </div>
     </div>
-    <div class="homeButton" @click.prevent.left="redirecting()">
+    <div class="homebutton" @click.prevent.left="$router.push('/Home')">
       <GoButton text="header.home" :colore="'#B1B9FC'"/>
     </div>
   </div>
 </template>
-
 <script>
 import MainHeader from "@/components/MainHeader.vue";
-import SearchBar from "@/components/SearchBar.vue";
 import GoButton from "@/components/GoButton.vue";
+import Swal from 'sweetalert2';
 import GlobalMethods from "@/components/GlobalMethods.vue";
 export default {
-  components: {
-    MainHeader,
-    SearchBar,
+  components : {
     GoButton,
-  },
-  methods: {
-    /*Méthode pour rediriger vers la page d'accueil*/
-    redirecting() {
-      GlobalMethods.isAClient();
-    }
+    MainHeader,
   },
   /*Méthode pour charger la langue sauvegardée en cookie*/
   mounted() {
@@ -38,35 +41,89 @@ export default {
       this.$cookies.set("lang", this.$i18n.locale)
     }
   },
-  props: {
-    contract: [
-      {
-        name: "Dupuis",
-        EAN: "1234567890123",
-        date: "01/01/2020",
-      },
-      {
-        name: "Bourgies",
-        EAN: "2356789012345",
-        date: "02/02/2020",
-      },
-      {
-        name: "D'Haene",
-        EAN: "3456789012345",
-        date: "03/03/2020",
-      }
-    ]
+  data(){
+    return{
+      linkApi : "https://babawallet.alwaysdata.net/api/client/contracts/",
+      nbr : 1,
+      loading : false,
+      lastPage : 0,
+      listContracts: []
+    }},
+  /*Au moment de la création on récupère déjà la première page de l'api*/
+  created() {
+    this.getPage();
   },
-};
+  methods: {
+    /*Méthode permettant de récupérer les pages des wallets de l'Api en scrollant */
+    async getPage(){
+      const requestOptions = {
+        method: "GET",
+        headers: {'Authorization' : this.$cookies.get("token")},
+      };
+      this.loading = true; //bloquer les demandes de loader pendant ce temps.
+      try {
+        const response = await fetch(`${this.linkApi}page?page=${this.nbr}&limit=3`, requestOptions);
+        if (!response.ok) {
+          const data = await response.text();
+          if(response.status == 401 && data.trim() === ''){
+            throw new Error("Token");
+          }
+          else{
+            const data = await response.json();
+            throw new Error(data.error);
+          }
+        } else {
+          const data = await response.json();
+          this.listWallet.push(data.contracts); //ajouter la suite de la réponse à la liste
+          this.lastPage = data.last_page;
+        }
+        this.loading = false;
+      } catch(error) {
+        if(error.message === "Token") {
+          this.$cookies.remove("token");
+          this.$cookies.remove("role");
+          Swal.fire('Your connection has expired');
+          this.$router.push("/");
+        }
+        else {
+          if(this.nbr == 1){
+            this.loading = true;
+            Swal.fire('No contracts');
+          }
+          else{
+            GlobalMethods.errorApi(error.message);
+          }
+        }
+      }
+    },
+    /*Lorsque l'utilisateur scrolle, cette méthode est appelée
+    pour augmenter le nombre de la page et appeler getPage*/
+    loader()
+    {
+      if(!this.loading)
+      {
+        this.nbr++;
+        this.getPage();
+      }
+    },
+    /*Méthode permettant de vérifier si la dernière page n'a pas encore été chargée
+    et si on est pas en cours de chargement*/
+    notLastPage(){
+      if(this.lastPage == this.nbr || this.loading == true){
+        return false;
+      }
+      return true;
+    },
+    /*On sauvegarde l'adresse du wallet sur lequel on souhaite plus d'informations
+    et on redirige vers walletFull*/
+    seeMore(wallet){
+      sessionStorage.setItem('address', wallet.address);
+    }
+  }
+}
 </script>
 
 <style scoped>
-.main {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  height: 100vh;
-}
 
 .header {
   display: flex;
@@ -74,8 +131,48 @@ export default {
   justify-content: center;
 }
 
-.homeButton {
+.main {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 100vh;
+}
+
+.homebutton {
   display: flex;
   justify-content: center;
+}
+
+.allcards {
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+
+.cards {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 250px;
+  height: 400px;
+  box-shadow: 0 15px 50px rgba(177, 185, 252, 1);
+  margin: 20px;
+  border-radius: 30px;
+}
+
+.texte {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 50px;
+}
+
+.name {
+  color: rgb(138, 150, 253);
+  font-size: 30px;
 }
 </style>
