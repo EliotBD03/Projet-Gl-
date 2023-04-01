6a9db0c6144d3db0b78bfeaed3072005ca824326
EliotBD03 <julienladeuze@outlook.fr>
2023-04-01 11:49:57 +0200

    begin of ContractFull

---
+++ Site/src/views/ContractFull.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="main">
+        <div class="header">
+            <MainHeader :text="proposal.proposalName"/>
+        </div>
+        <div class="list">
+            <p>Information : </p>
+            <p>Provider : {{ proposal.nameProvider }}</p>
+            <p>Energy : {{ proposal.typeEnergy }}</p>
+        </div>
+    </div>
+</template>
