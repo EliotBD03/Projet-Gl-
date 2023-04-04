b78a50afe6b9390fba2edaa503050d42ef073f90
Maxime Dupuis <212107@umons.ac.be>
2023-04-04 20:19:37 +0200

    Refactor NewContractsPage

--- Site/src/views/NewContractsPage.vue
+++ Site/src/views/NewContractsPage.vue
@@ -1,52 +1,53 @@
-
 <template>
-  <div class="main">
-    <div class="header">
-      <MainHeader text="header.newcontracts"/>
-    </div>
-    <div class="container">
-      <div class="list">
+    <div class="main">
+        <div class="header">
+            <MainHeader text="header.newcontracts"/>
+        </div>
         <div class="proposals">
-          <p>PROPOSALS</p>
-          <div v-for="(proposal,index) in proposals" :key="proposal.id">
-            <li v-for="location in locations[index]" :key="location.id">
-              {{ location }}
-            </li>
-            <p>Provider name: {{ proposal.nameProvider }}</p>
-            <p>Offer name: {{ proposal.proposalName }}</p>
+            <div class="cards" v-for="proposal in proposals" :key="proposal.id">
+                <div class="text">
+                    <p><b>Location :</b></p>
+                    {{ convertLocation(proposal.location) }}
+                </div>
+                <p><b>Provider name:</b></p>
+                {{ proposal.nameProvider }}
+                <p><b>Offer name: </b></p>
+                {{ proposal.proposalName }}
             <div @click.prevent.left="seeMore(proposal)">
-              <GoButton text="button.go" :colore="'#34c98e'"/>
+                <GoButton text="button.go" :colore="'#34c98e'"/>
             </div>
-          </div>
-          <div v-if="notLastPage()" @click.prevent.left="loader()">
-            <GoButton text="See more proposals" :colore="'#B1B9FC'"/>
-          </div>
-
-          <div class="checkboxleftside">
-            <p>LOCATION</p>
-            <span v-for="location in allLocations" :key="location.id">
+            </div>
+        </div>
+    <div class="checkboxleftside">
+        <div class="filters">
+            <div class="location">
+                <p><b>LOCATION</b></p>
+                <span v-for="location in allLocations" :key="location.id">
               <input type="checkbox" :value="location.value" v-model="selectedLocations" v-on:click="checkOnlyOneLocation()">
               <span class="checkbox-label">{{ location.name }}</span> <br>
             </span>
-            <p>ENERGY</p>
-            <span v-for="energy in allEnergies" :key="energy.id">
+            </div>
+            <div class="energy">
+                <p><b>ENERGY</b></p>
+                <span v-for="energy in allEnergies" :key="energy.id">
               <input type="checkbox" :value="energy.name" v-model="selectedEnergies" v-on:click="checkOnlyOneEnergy()">
-              <span class="checkbox-label">{{ energy.name }}</span>
+              <span class="checkbox-label">{{ energy.name }}</span> <br>
             </span>
-            <div class="applyFilterButton" @click.prevent.left="applyFilter()">
-              <GoButton text="Apply filter(s)" :colore="'#2962FF'"/>
             </div>
         </div>
-        <div class="homeButton" @click.prevent.left="$router.push('/Home')">
-          <GoButton text="header.home" :colore="'#B1B9FC'"/>
+        <div class="applyFilterButton" @click.prevent.left="applyFilter()">
+            <GoButton text="Apply filter(s)" :colore="'#2962FF'"/>
         </div>
-        </div>
-      </div>
-      
     </div>
-
-    
-  </div>
+    <div class="bottombuttons">
+        <div class="homeButton" @click.prevent.left="$router.push('/Home')">
+            <GoButton text="header.home" :colore="'#B1B9FC'"/>
+        </div>
+        <div v-if="notLastPage()" @click.prevent.left="loader()">
+            <GoButton text="See more proposals" :colore="'#B1B9FC'"/>
+        </div>
+    </div>
+    </div>
 </template>
 
 <script>
@@ -55,198 +56,266 @@ import GoButton from "@/components/GoButton.vue";
 import  Swal  from "sweetalert2";
 import GlobalMethods from "@/components/GlobalMethods.vue";
 export default {
-  components: {
-    MainHeader,
-    GoButton,
-  },
-  /*Méthode pour charger la langue sauvegardée en cookie*/
-  mounted() 
-  {
-    if (this.$cookies.get("lang")) 
-    {
-      this.$i18n.locale = this.$cookies.get("lang");
-    } 
-    else 
-    {
-      this.$cookies.set("lang", this.$i18n.locale)
-    }
-  },
-  data(){
-    return{
-      linkApi: "https://babawallet.alwaysdata.net/api/client/proposals/",
-      nbr: 1,
-      proposals:[],
-      locations:[],
-      loading: false,
-      lastPage: 0,
-      selectedLocations : [], //used to filter the location
-      selectedEnergies : [], //used to filter the energies
-      allLocations: 
-      [
-        {name: "Brussels", value: "100"},
-        {name: "Flanders", value: "010"},
-        {name: "Wallonia", value: "001"}
-      ],
-      allEnergies:
-      [
-        {name: "electricity"},
-        {name: "water"},
-        {name: "gas"}
-      ]
-    }
-  },
-  created(){
-    this.getPage();
-  },
-  methods:
-  {
-    async getPage()
-    {
-      const requestOptions = 
-      {
-        method: "GET",
-        headers: {'Authorization' : this.$cookies.get("token")},
-      };
-      this.loading = true;
-      try
-      {
-        var query = `${this.linkApi}page?page=${this.nbr}&energy_category=${this.selectedEnergies}&region_category=${this.selectedLocations}&limit=2`;
-        console.log(this.selectedEnergies, this.selectedLocations);
-        if(this.selectedEnergies.length == 0 && this.selectedLocations.length == 0)
-          query = `${this.linkApi}page?page=${this.nbr}&limit=2`;
-        else if(this.selectedEnergies.length == 0)
-          query = `${this.linkApi}page?page=${this.nbr}&region_category=${this.selectedLocations}&limit=2`;
-        else if(this.selectedLocations.length == 0)
-          query = `${this.linkApi}page?page=${this.nbr}&energy_category=${this.selectedEnergies}&limit=2`;
-
-        const response = await fetch(query, requestOptions);
-        if(!response.ok)
-        {
-          if(response.status == 401)
-          {
-            throw new Error("Token");
-          }
-          else
-          {
-            const data = await response.json();
-            throw new Error(data.error);
-          }
-        }
-        else
-        {
-          const data = await response.json();
-          this.lastPage = data.last_page;
-          if(this.lastPage == 0)
-          {
-            this.loading = true;
-            Swal.fire("No proposal");
-          }
-          else if(this.lastPage >= this.nbr)
-          {
-            this.proposals.push(data.proposals);
-            this.proposals = this.proposals.flat();
-            this.loading = false;
-
-            var temp = this.proposals;
-            var actualLoc = [];
-            const references = ["Brussels-Capital", "Flanders", "Wallonia"];
-            for(var i = 0; i < temp.length; i++)
-            {
-              let current = [];
-              for(var j = 0; j <= 3; j++)
-              {
-                if(temp[i].location.substring(j,j + 1) == "1")
-                  current.push(references[j]);
-              }
-              actualLoc.push(current)
-            }
-            this.locations = actualLoc;
-          }
-        }
-      }
-      catch(error)
-      {
-        if(error.message === "Token")
-        {
-          this.$cookies.remove("token");
-          this.$cookies.remove("role");
-          Swal.fire('Your connection has expired');
-          this.$router.push("/");
-        }
-        else
-        {
-          GlobalMethods.errorApi(error.message);
-        }
-      }
+    components: {
+        MainHeader,
+        GoButton,
     },
-    loader()
+    /*Méthode pour charger la langue sauvegardée en cookie*/
+    mounted()
     {
-      if(!this.loading)
-      {
-        this.nbr++;
-        this.proposal = [];
+        if (this.$cookies.get("lang"))
+        {
+            this.$i18n.locale = this.$cookies.get("lang");
+        }
+        else
+        {
+            this.$cookies.set("lang", this.$i18n.locale)
+        }
+    },
+    data(){
+        return{
+            linkApi: "https://babawallet.alwaysdata.net/api/client/proposals/",
+            nbr: 1,
+            proposals:[],
+            locations:[],
+            loading: false,
+            lastPage: 0,
+            selectedLocations : [], //used to filter the location
+            selectedEnergies : [], //used to filter the energies
+            allLocations:
+                [
+                    {name: "Brussels", value: "100"},
+                    {name: "Flanders", value: "010"},
+                    {name: "Wallonia", value: "001"}
+                ],
+            allEnergies:
+                [
+                    {name: "electricity"},
+                    {name: "water"},
+                    {name: "gas"}
+                ]
+        }
+    },
+    created(){
         this.getPage();
-      }
     },
-    notLastPage()
-    {
-      if(this.lastPage == this.nbr || this.loading == true)
-        return false;
-      return true;
-    },
-    seeMore(proposal)
-    {
-      console.log(proposal)
-      sessionStorage.setItem('providerId', proposal.providerId) 
-      sessionStorage.setItem('proposalName', proposal.proposalName) 
-      this.$router.push('/contractInformation');
-    },
-    applyFilter()
-    {
-      this.proposals = []
-      this.getPage();
-    },
-    checkOnlyOneEnergy()
-    {
-      this.selectedEnergies = [];
-    },
-    checkOnlyOneLocation()
-    {
-      this.selectedLocations = [];
-    }
-  }
+    methods:
+        {
+            async getPage()
+            {
+                const requestOptions =
+                    {
+                        method: "GET",
+                        headers: {'Authorization' : this.$cookies.get("token")},
+                    };
+                this.loading = true;
+                try
+                {
+                    var query = `${this.linkApi}page?page=${this.nbr}&energy_category=${this.selectedEnergies}&region_category=${this.selectedLocations}&limit=2`;
+                    console.log(this.selectedEnergies, this.selectedLocations);
+                    if(this.selectedEnergies.length == 0 && this.selectedLocations.length == 0)
+                        query = `${this.linkApi}page?page=${this.nbr}&limit=2`;
+                    else if(this.selectedEnergies.length == 0)
+                        query = `${this.linkApi}page?page=${this.nbr}&region_category=${this.selectedLocations}&limit=2`;
+                    else if(this.selectedLocations.length == 0)
+                        query = `${this.linkApi}page?page=${this.nbr}&energy_category=${this.selectedEnergies}&limit=2`;
+
+                    const response = await fetch(query, requestOptions);
+                    if(!response.ok)
+                    {
+                        if(response.status == 401)
+                        {
+                            throw new Error("Token");
+                        }
+                        else
+                        {
+                            const data = await response.json();
+                            throw new Error(data.error);
+                        }
+                    }
+                    else
+                    {
+                        const data = await response.json();
+                        this.lastPage = data.last_page;
+                        if(this.lastPage == 0)
+                        {
+                            this.loading = true;
+                            Swal.fire("No proposal");
+                        }
+                        else if(this.lastPage >= this.nbr)
+                        {
+                            this.proposals.push(data.proposals);
+                            this.proposals = this.proposals.flat();
+                            this.loading = false;
+
+                            var temp = this.proposals;
+                            var actualLoc = [];
+                            const references = ["Brussels-Capital", "Flanders", "Wallonia"];
+                            for(var i = 0; i < temp.length; i++)
+                            {
+                                let current = [];
+                                for(var j = 0; j <= 3; j++)
+                                {
+                                    if(temp[i].location.substring(j,j + 1) == "1")
+                                        current.push(references[j]);
+                                }
+                                actualLoc.push(current)
+                            }
+                            this.locations = actualLoc;
+                        }
+                    }
+                }
+                catch(error)
+                {
+                    if(error.message === "Token")
+                    {
+                        this.$cookies.remove("token");
+                        this.$cookies.remove("role");
+                        Swal.fire('Your connection has expired');
+                        this.$router.push("/");
+                    }
+                    else
+                    {
+                        GlobalMethods.errorApi(error.message);
+                    }
+                }
+            },
+            loader()
+            {
+                if(!this.loading)
+                {
+                    this.nbr++;
+                    this.proposal = [];
+                    this.getPage();
+                }
+            },
+            notLastPage()
+            {
+                if(this.lastPage == this.nbr || this.loading == true)
+                    return false;
+                return true;
+            },
+            seeMore(proposal)
+            {
+                console.log(proposal)
+                sessionStorage.setItem('providerId', proposal.providerId)
+                sessionStorage.setItem('proposalName', proposal.proposalName)
+                this.$router.push('/contractInformation');
+            },
+            applyFilter()
+            {
+                this.proposals = []
+                this.getPage();
+            },
+            checkOnlyOneEnergy()
+            {
+                this.selectedEnergies = [];
+            },
+            checkOnlyOneLocation()
+            {
+                this.selectedLocations = [];
+            },
+            convertLocation: function(location) {
+                const result = [];
+
+                if (location >= 100) {
+                    result.push('Wallonie');
+                    location -= 100;
+                }
+
+                if (location >= 10) {
+                    result.push('Flandre');
+                    location -= 10;
+                }
+
+                if (location >= 1) {
+                    result.push('Bruxelles-Capitale');
+                }
+
+                return result.join(' - ');
+            },
+        }
 };
 </script>
 
 <style scoped>
 .main {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    height: 100vh;
 }
+
+.proposals {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+
 .header {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 9999; 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 9999;
 }
-.container {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
+
+.checkboxleftside {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 }
-.list {
-  display: flex;
-  align-items: center;
-  justify-content: center;
+
+.bottombuttons {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;
 }
-.checkboxleftside{
-  float: left;
-  display: relative;
-  left: 0%;
-  flex-direction: column;
-  padding: 0 50px;
-  margin-left: 10 10px;
+
+.location {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 20px;
+}
+
+.energy {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 20px;
+}
+
+.filters {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin: 20px;
+}
+
+.cards {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 250px;
+    height: 400px;
+    box-shadow: 0 15px 50px rgba(177, 185, 252, 1);
+    margin: 10px;
+    border-radius: 30px;
+}
+
+.text {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
 }
 </style>
