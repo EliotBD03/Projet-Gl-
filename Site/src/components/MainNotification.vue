4ad25b6061f859d7e55d4035a55d8706d412cc12
ClaireDH3 <clairedhaene3@gmail.com>
2023-04-20 13:06:03 +0200

    Réglé langues dans notifs

--- Site/src/components/MainNotification.vue
+++ Site/src/components/MainNotification.vue
@@ -8,8 +8,8 @@
             </div>
             <div class="p">{{  $t("notifications.text",{text: texte}) }}</div>
             <div class="fields">
-                <input type="text" v-model="ean" v-if="checkStatus()" placeholder="Code EAN">
-                <input type="text" v-model="adress" v-if="checkStatus()" placeholder="Adress">
+                <input type="text" v-model="ean" v-if="checkRole()" :placeholder="$t('client.eancode')">
+                <input type="text" v-model="adress" v-if="checkRole()" :placeholder="$t('proposal.address')">
             </div>
             <div>
             </div>
@@ -17,18 +17,18 @@
         <div class="statesButtons">
             <div class="interactbutton" v-if="checkStatus()">
                 <div class="acceptbutton" @click.prevent.left="accept(id_notification)">
-                    ACCEPT
+                    {{ $t("button.accept") }}
                 </div>
                 <div class="refusebutton" @click.prevent.left="refuse(id_notification)">
-                    REFUSE
+                    {{ $t("button.deny") }}
                 </div>
                 <div class="seebutton" @click.prevent.left="seeProposal(proposalName)">
-                    INFOS
+                    {{ $t("button.infos") }}
                 </div>
             </div>
             <div class="deletedbutton" v-else>
                 <div class="seenbutton" @click.prevent.left="deleted(id_notification)">
-                    SEEN
+                    {{ $t("button.seen") }}
                 </div>
             </div>
         </div>
@@ -39,7 +39,7 @@
 import moment from "moment";
 export default {
     name: "MainNotification",
-    props: ["time","text","color","id_notification","proposalName", "providerId"],
+    props: ["time","text","color","id_notification","proposalName", "providerId","role"],
     data() {
         return {
             title: "",
@@ -56,11 +56,11 @@ export default {
             const elapsedHours = elapsedTime.asHours();
             const elapsedMinutes = elapsedTime.asMinutes();
             if (elapsedHours >= 1) {
-                return Math.floor(elapsedHours) + " hours ago";
+                return this.$t("notifications.time", [{time: Math.floor(elapsedHours), format: this.$t("settings.hours")}]);
             } else if (elapsedMinutes >= 1) {
-                return Math.floor(elapsedMinutes) + " minutes ago";
+                return this.$t("notifications.time", {time: (Math.floor(elapsedMinutes)), format: this.$t("settings.minutes")});
             } else {
-                return "Just now";
+                return this.$t("settings.now");
             }
         },
         updateTextAndTitle() {
@@ -68,21 +68,28 @@ export default {
             let texte = this.text;
             if (this.text.includes("accepted by")) {
                 title = this.text.split("accepted by")[1].trim();
-                texte = "Has accepted your contract";
+                texte = this.$t("notifications.accepted");
             } else if (this.text.includes("denied by")) {
                 title = this.text.split("denied by")[1].trim();
-                texte = "Has denied your contract";
+                texte = this.$t("notifications.denied");
             } else if (this.text.includes("from")) {
                 title = this.text.split("from")[1].trim();
-                texte = "Contract request";
+                texte = this.$t("notifications.request");
             } else if (this.text.includes("daily consumption")) {
                 const regex = /in the (.+) has changed to (.+) for this ean code : (.+)/;
                 const match = regex.exec(this.text);
                 const date = moment(match[1]).format("YYYY-MM-DD");
                 const consumption = match[2];
                 const ean = match[3];
-                title = "System";
-                texte = `Daily consumption (${date}) has changed to ${consumption} (EAN: ${ean}).`; }
+                title = this.$t("notifications.system");
+                texte = this.$t("notifications.dailychange", {date: date, consumption: consumption, ean: ean});
+            } else if (this.text.includes("deleted by")) {
+                title = this.text.split(":")[1].trim();
+                texte = this.$t("notifications.deleted");
+            } else if (this.text.includes("expired")) {
+                title = this.$t("notifications.system");
+                texte = this.$t("notifications.expired");
+            }
             this.title = title;
             this.texte = texte;
         },
@@ -96,14 +103,16 @@ export default {
             this.$emit("delete", this.id_notification);
         },
         seeProposal() {
-           if (this.role === "supplier") {
-               this.$emit("seeProposal", this.proposalName);
-           } else {
-                this.$emit("seeProposal", this.providerId, this.proposalName);
-           }
+            this.$emit("seeProposal", this.providerId, this.proposalName);
         },
         checkStatus() {
-            return !!this.text.includes("request");
+            return this.text.includes("request");
+        },
+        checkRole() {
+            if (this.role === 'client') {
+                return this.checkStatus();
+            }
+            else return false;
         }
     },
     mounted() {
@@ -120,16 +129,14 @@ export default {
     border-radius: 20px;
     display: flex;
     align-items: center;
-    justify-content: left;
+    justify-content: center;
     backdrop-filter: blur(10px);
     transition: 0.5s ease-in-out;
 }
-
 .card:hover {
     cursor: pointer;
     transform: scale(1.05);
 }
-
 .img {
     width: 50px;
     height: 50px;
@@ -137,7 +144,6 @@ export default {
     border-radius: 10px;
     background: linear-gradient(#d7cfcf, #9198e5);
 }
-
 .card:hover > .img {
     transition: 0.5s ease-in-out;
     background-repeat: no-repeat;
@@ -145,35 +151,29 @@ export default {
     background-size: 50px;
     background: linear-gradient(#3348f3, #9198e5);
 }
-
 .textBox {
     width: calc(100% - 90px);
     margin-left: 10px;
     color: white;
 }
-
 .textContent {
     display: flex;
     align-items: center;
     justify-content: space-between;
 }
-
 .span {
     font-size: 10px;
 }
-
 .h1 {
     font-size: 16px;
     font-weight: bold;
 }
-
 .p {
     font-size: 12px;
     font-weight: lighter;
     display: flex;
     align-items: center;
 }
-
 .statesButtons {
     display: flex;
     align-items: center;
@@ -181,7 +181,6 @@ export default {
     flex-direction: column;
     margin: 20px;
 }
-
 .acceptbutton {
     width: 50px;
     height: 20px;
@@ -194,7 +193,6 @@ export default {
     color: white;
     margin-bottom: 10px;
 }
-
 .refusebutton {
     width: 50px;
     height: 20px;
@@ -206,7 +204,6 @@ export default {
     font-size: 10px;
     color: white;
 }
-
 .seenbutton {
     width: 50px;
     height: 20px;
@@ -218,7 +215,6 @@ export default {
     font-size: 10px;
     color: white;
 }
-
 .seebutton {
     width: 50px;
     height: 20px;
@@ -231,14 +227,12 @@ export default {
     color: white;
     margin: 5px;
 }
-
 .interactbutton {
     display: flex;
     align-items: center;
     justify-content: center;
     flex-direction: column;
 }
-
 .fields {
     display: flex;
     align-items: center;
