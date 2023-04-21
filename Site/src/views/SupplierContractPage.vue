ae8837d967a859c6a44b5a0fedfca8c0673c6695
fadri14 <fievet.adrien2003@gmail.com>
2023-04-21 09:49:30 +0200

    mis a jour de la modification de proposition

--- Site/src/views/SupplierContractPage.vue
+++ Site/src/views/SupplierContractPage.vue
@@ -1,204 +1,192 @@
 <template>
     <div class="main">
         <div class="header">
-            <MainHeader text="header.contracts"/>
+            <MainHeader text="header.modifyproposal"/>
         </div>
-        <div class="allcards">
-            <div class=cards v-for="contract in listContracts" :key="contract.id">
-                <div class="texte">
-                    <p class="name"> {{ contract.proposalName }}</p>
-                    <p><b>{{ $t("account.provider") }} :</b></p>
-                    <p> {{ contract.nameProvider }}</p>
-                    <p><b>{{ $t("proposal.typeofenergy") }} :</b></p>
-                    <p> {{ contract.typeOfEnergy.charAt(0).toUpperCase() + contract.typeOfEnergy.slice(1) }}</p>
-                    <p><b>{{ $t("proposal.location") }} :</b></p>
-                    <p>{{ convertLocation(contract.location) }}</p>
-                </div>
-                <div @click.prevent.left="seeMore(contract)">
-                    <GoButton text="button.go" :colore="'#34c98e'"/>
-                </div>
-            </div>
-            <div v-if="notLastPage()" @click.prevent.left="loader()">
-                <GoButton text="button.seemore" :colore="'#B1B9FC'"/>
+        <div class="contact-form">
+            <div class="content">
+                <form id="addWallet" method="post" v-on:submit.prevent="post">
+                    <p v-if="!contract.fixedRate">
+                        {{ $t("proposal.priceperday") }} :
+                        <InputMain :text="$t('proposal.priceperday')" v-model="variable_day_price"/>
+                    </p>
+                    <p v-if="checkCounter()">
+                        {{ $t("proposal.pricepernight") }} :
+                        <InputMain :text="$t('proposal.pricepernight')" v-model="variable_night_price"/>
+                    </p>
+                    <div v-if="checkCounter()">
+                        <p>
+                            {{ $t("proposal.peakhours") }} :
+                        </p>
+                        <label for="start-time">{{ $t("proposal.starthour") }} : </label>
+                        <select id="start-time" v-model="start_off_peak_hours">
+                            <option v-for="hour in hours" :key="hour">{{ hour }}</option>
+                        </select>
+                        <label for="end-time"> {{ $t("proposal.endhour") }} : </label>
+                        <select id="end-time" v-model="end_off_peak_hours">
+                            <option v-for="hour in hours" :key="hour">{{ hour }}</option>
+                        </select>
+                    </div>
+                    <GoButton text="button.change" type="submit" :colore="'green'"/>
+                </form>
             </div>
         </div>
-        <div class="bottombuttons">
-            <div class="homebutton" @click.prevent.left="redirecting()">
-                <GoButton text="header.home" :colore="'#B1B9FC'"/>
-            </div>
-            <div class="addbutton" @click.prevent.left="$router.push('/createContract')">
-                <GoButton text="button.add" :colore="'#34c98e'"/>
-            </div>
+        <div class="backbutton" @click.prevent.left="back()">
+            <GoButton text="button.back" :colore="'darkblue'"/>
         </div>
     </div>
 </template>
+
 <script>
-import MainHeader from "@/components/MainHeader.vue";
 import GoButton from "@/components/GoButton.vue";
 import Swal from 'sweetalert2';
+import InputMain from "@/components/InputMain.vue";
+import MainHeader from "@/components/MainHeader.vue";
 import GlobalMethods from "@/components/GlobalMethods.vue";
 export default {
-    components : {
-        GoButton,
-        MainHeader,
-    },
+    components: {InputMain, GoButton, MainHeader},
     data(){
         return{
-            linkApi : "https://babawallet.alwaysdata.net/api/provider/proposals/",
-            nbr : 1,
-            loading : false,
-            lastPage : 0,
-            listContracts: [],
-            location: "",
+            name_proposal: sessionStorage.getItem('name_proposal'),
+            contract: [],
+            variable_night_price: '',
+            variable_day_price: '',
+            hours: [], // tableau des heures disponibles
+            start_off_peak_hours: null, // heure de début sélectionnée
+            end_off_peak_hours: null // heure de fin sélectionnée
         }},
-    /*Au moment de la création on récupère déjà la première page de l'api*/
+    mounted() {
+        // génération du tableau des heures disponibles
+        for (let i = 0; i < 24; i++) {
+            const hour = i < 10 ? `0${i}` : `${i}`;
+            this.hours.push(`${hour}:00:00`);
+        }
+    },
     created() {
-        this.getPage();
+        this.getProposal();
         GlobalMethods.getCurrentLanguage();
     },
+    /*Méthode qui vérifie si les champs sont bien remplis sinon envoie une pop-up*/
     methods: {
-        /*Méthode permettant de récupérer les pages des wallets de l'Api en scrollant */
-        async getPage(){
+        async getProposal() {
             const requestOptions = {
-                method: "GET",
-                headers: {'Authorization' : this.$cookies.get("token")},
+                method: 'GET',
+                headers: {'Authorization' : this.$cookies.get('token')}
             };
-            this.loading = true; //bloquer les demandes de loader pendant ce temps.
             try {
-                const response = await fetch(`${this.linkApi}page?page=${this.nbr}&limit=3`, requestOptions);
+                console.log(this.name_proposal);
+                const response = await fetch(`https://babawallet.alwaysdata.net/api/provider/proposals/${this.name_proposal}`,requestOptions);
                 if (!response.ok) {
                     const data = await response.json();
                     throw new Error(data.error);
-                } else {
-                    const data = await response.json();
-                    this.lastPage = data.last_page;
-                    if(this.lastPage == 0){
-                        this.loading = true;
-                        Swal.fire(this.$t("alerts.nocontracts"));
-                    }
-                    else if(this.lastPage >= this.nbr){
-                        this.listContracts.push(data.allProposals); //ajouter la suite de la réponse à la liste
-                        this.listContracts = this.listContracts.flat(); //transforme une liste multidimensionnelle en une liste à une seule dimension
-                        this.loading = false;
-                    }
-                }
-            } catch(error) {
-                if(error.message === "error.unauthorizedAccess") {
-                    GlobalMethods.errorToken();
                 }
                 else {
-                    if(this.nbr === 1){
-                        this.loading = true;
-                        Swal.fire(this.$t("alerts.nocontracts"));
-                    }
-                    else{
-                        GlobalMethods.errorApi(error.message);
-                    }
+                    const data = await response.json();
+                    this.contract = data.proposal;
+                    this.variable_night_price = this.contract.variableNightPrice;
+                    this.variable_day_price = this.contract.variableDayPrice;
+                    this.start_off_peak_hours = this.contract.startOfPeakHours;
+                    this.end_off_peak_hours = this.contract.endOfPeakHours;
+                }
+            }
+            catch(error) {
+                if(error.error === "error.unauthorizedAccess")
+                        GlobalMethods.errorToken();
+                else {
+                    GlobalMethods.errorApi(error.message);
                 }
             }
         },
-        /*Lorsque l'utilisateur scrolle, cette méthode est appelée
-        pour augmenter le nombre de la page et appeler getPage*/
-        loader()
-        {
-            if(!this.loading)
-            {
-                this.nbr++;
-                this.getPage();
+        checkCounter() {
+            return parseFloat(this.variable_night_price) !== 0;
+        },
+        checkArgs() {
+            if (this.variable_night_price === '' && this.checkCounter()) Swal.fire(this.$t("alerts.enternightprice"));
+            else if (!this.variable_day_price && !this.contract.fixedRate) Swal.fire(this.$t("alerts.enterdayprice"));
+            else if (this.checkCounter() && !this.start_off_peak_hours) Swal.fire(this.$t("alerts.selectstarthour"));
+            else if (this.checkCounter() && !this.end_off_peak_hours) Swal.fire(this.$t("alerts.selectendhour"));
+            else return true;
+        },
+        post() {
+            if (this.checkArgs()) {
+                const requestOptions = {
+                    method: "POST",
+                    body: JSON.stringify({
+                        name_proposal: this.name_proposal,
+                        type_of_energy: this.contract.typeOfEnergy,
+                        localization: this.contract.location,
+                        variable_night_price: parseFloat(this.variable_night_price),
+                        variable_day_price: parseFloat(this.variable_day_price),
+                        is_fixed_rate: this.contract.fixedRate,
+                        duration: this.contract.duration/720,
+                        start_off_peak_hours: this.start_off_peak_hours,
+                        end_off_peak_hours: this.end_off_peak_hours
+                    }),
+                    headers: {'Authorization' : this.$cookies.get("token")}
+                };
+                fetch("https://babawallet.alwaysdata.net/api/provider/proposals", requestOptions)
+                    .then(response => {
+                        if (!response.ok) {
+                            return response.json().then(json => Promise.reject(json));
+                        }
+                        return response.json();
+                    })
+                    .then(
+                        this.$router.push({name: 'HomeSupplier'}))
+                    .then(
+                        sessionStorage.removeItem('name_proposal')
+                    )
+                    .then(
+                        Swal.fire({
+                            icon: 'success',
+                            title: this.$t('alerts.good'),
+                            text: this.$t("alerts.modifiedproposal"),
+                        })
+                    ).catch(error => {
+                        if(error.error === "error.unauthorizedAccess")
+                            GlobalMethods.errorToken();
+                        else{
+                            GlobalMethods.errorApi();
+                        }
+                    });
             }
         },
-        convertLocation: function(location) {
-            const result = [];
-
-            if (location >= 100) {
-                result.push(this.$t("proposal.wallonia"));
-                location -= 100;
-            }
-
-            if (location >= 10) {
-                result.push(this.$t("proposal.flanders"));
-                location -= 10;
-            }
-
-            if (location >= 1) {
-                result.push(this.$t("proposal.brussels"));
-            }
-
-            return result.join(' - ');
+        back(){
+            sessionStorage.removeItem('name_proposal');
+            this.$router.push({name: 'ContractsSupplier'})
         },
-        notLastPage(){
-            if(this.lastPage == this.nbr || this.loading == true){
-                return false;
-            }
-            return true;
-        },
-        /*On sauvegarde l'adresse du wallet sur lequel on souhaite plus d'informations
-        et on redirige vers walletFull*/
-        seeMore(contract){
-            sessionStorage.setItem('name_proposal', contract.proposalName);
-            this.$router.push({name: 'ProposalFull'})
-        },
-        redirecting(){
-            GlobalMethods.isAClient();
-        }
     }
 }
 </script>
 
 <style scoped>
-
-.header {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
 .main {
     display: flex;
     flex-direction: column;
     justify-content: space-evenly;
+    align-items: center;
     height: 100vh;
 }
-
-
-.allcards {
+.header {
     display: flex;
-    align-items: center;
-    flex-direction: row;
-    justify-content: space-evenly;
-}
-
-.cards {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-    width: 250px;
-    height: 500px;
-    box-shadow: 0 15px 50px rgba(177, 185, 252, 1);
-    margin: 10px;
-    border-radius: 30px;
-}
-
-.texte {
-    display: flex;
-    flex-direction: column;
     align-items: center;
     justify-content: center;
-    margin: 10px;
+    z-index: 9999;
+    padding: 10px;
 }
-
-.name {
-    color: rgb(138, 150, 253);
-    font-size: 30px;
-}
-
-.bottombuttons {
+.contact-form{
     display: flex;
-    flex-direction: row;
     align-items: center;
-    justify-content: space-between;
-    padding: 0 50px;
-    margin-top: 10px;
+    justify-content: space-evenly;
+    flex-direction: column;
+    width: 600px;
+    height: fit-content;
+    border-radius: 50px;
+    background: #e0e0e0;
+    box-shadow: 0 15px 50px rgba(177, 185, 252, 1);
+}
+.content {
+    margin-top: 20px;
+    margin-bottom: 20px;
 }
 </style>
